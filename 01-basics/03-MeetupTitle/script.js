9e81508cd16c702f2dff4bc4cf9265a707b2fdb9
Karina Hubskaya <karinagubskaya0895@gmail.com>
2021-05-19 19:29:10 +0300

    fixed meetup title task, but didn't find reasons to use watch in this case

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -3,14 +3,24 @@ import Vue from './vendor/vue.esm.browser.js';
 const fetchMeetups = () =>
   fetch('https://course-vue.javascript.ru/api/meetups/').then((res) => res.json());
 
+
+const radioButtons =  [1, 2, 3, 4, 5];
+
 // Создаём новое Vue приложением через конструктор, передавая опции, описывающие приложение
 const app = new Vue({
   // Шаблон в #app
   template: `#app`,
   data() {
     return {
+        radioButtons,
         rawMeetups: null,
         radioTitle: ' ',
+    }
+  },
+  methods: {
+    buttonChangeTitle (e) {
+     let buttonValue =  e.target.value;
+     return this.radioTitle = this.rawMeetups[buttonValue].title
     }
   },
   mounted() {
