220bc49078eead5a1f6de91b0431e76d3d94b42f
Karina <karinagubskaya0895@gmail.com>
2021-06-06 20:43:55 +0300

    meetup cover component

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,47 @@
 export default {
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true
+    },
+    place: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    }
+  },
+
+  computed: {
+    formatDate() {
+      return new Date(this.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+    dateOnlyString(){
+      return new Date(this.date).toISOString().substr(0, 10)
+    } 
+  },
+
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="dateOnlyString">{{ formatDate }}</time>
       </li>
     </ul>`,
 };
