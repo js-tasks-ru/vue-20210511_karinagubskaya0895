b83fcb7725f70de28b0af394424c77d243c9bd26
Karina <karinagubskaya0895@gmail.com>
2021-06-06 20:09:34 +0300

    meetup cover component

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,9 +1,18 @@
 const MeetupCover = {
   name: 'MeetupCover',
 
+  props: {
+    link: {
+      type: String
+    }, 
+    title: {
+      type: String
+    }
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="link && { '--bg-url': \`url('\${link}')\` }">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 };
 
