ac64721c8e2604c989d938264f66a8e8dfb925bb
Karina Hubskaya <karinagubskaya0895@gmail.com>
2021-07-02 16:56:15 +0300

    07 meetup page component

--- 02-components/07-MeetupPageComponent/MeetupPage.js
+++ 02-components/07-MeetupPageComponent/MeetupPage.js
@@ -1,10 +1,25 @@
-// import MeetupView from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import MeetupView from './MeetupView.js';
+import { MEETUP_ID, fetchMeetup } from './data.js';
 
 const MeetupPage = {
+  data() {
+    return {
+      fetchMeetup,
+      MEETUP_ID,
+      meetup: null,
+    };
+  },
+  components: {
+    MeetupView,
+  },
+  mounted() {
+    fetchMeetup(MEETUP_ID).then((meetup) => {
+      this.meetup = meetup;
+    });
+  },
   name: 'MeetupPage',
-
-  template: `<div></div>`,
+  template: `<div v-if="meetup">
+    <meetup-view :meetup="meetup"></meetup-view>
+  </div>`,
 };
-
 export default MeetupPage;
---
+++ 02-components/07-MeetupPageComponent/MeetupView.js
@@ -0,0 +1,45 @@
+import MeetupCover from './MeetupCover.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupAgenda from './MeetupAgenda.js';
+import MeetupInfo from './MeetupInfo.js';
+import { getImageUrlByImageId } from './data.js';
+
+const MeetupView = {
+  data() {
+    return {
+      getImageUrlByImageId,
+    };
+  },
+  name: 'MeetupView',
+  components: {
+    MeetupAgenda,
+    MeetupCover,
+    MeetupDescription,
+    MeetupInfo,
+  },
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+  template: `
+    <div>
+      <meetup-cover :link="meetup.imageId && getImageUrlByImageId(meetup.imageId)" :title="meetup.title"></meetup-cover>
+      <div class="container">
+        <div class="meetup">
+          <div class="meetup__content">
+            <h3>Описание</h3>
+            <meetup-description :description="meetup.description"></meetup-description>
+            <h3>Программа</h3>
+            <meetup-agenda :agenda="meetup.agenda"></meetup-agenda>
+          </div>
+          <div class="meetup__aside">
+            <meetup-info :date="new Date(meetup.date)" :organizer="meetup.organizer" :place="meetup.place"></meetup-info>
+          </div>
+        </div>
+      </div>
+    </div>`,
+};
+
+export default MeetupView;
